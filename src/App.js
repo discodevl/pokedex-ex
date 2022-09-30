ff3a0b1395772489f064711ca3ab2a9a138effa6
discodevl <donato.cutro3@gmail.com>
2022-09-30 18:28:51 +0200

    set up api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import { useEffect } from "react";
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon/?offset=20')//40//60...
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
