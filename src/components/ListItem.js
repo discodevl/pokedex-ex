68650006acd6349076e495a3b59c822a45288756
discodevl <donato.cutro3@gmail.com>
2022-10-02 04:26:45 +0200

    styles

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -23,46 +23,13 @@ function ListItem({ url, data, children }) {
   function toggleModal() {
     setIsOpenModal((modal) => !modal);
   }
-
-  // function getTypeColor() {
-  //   switch (pokemon.types[0].type.name) {
-  //     case "bug":
-  //       return "rgba(28, 75, 39, 0.5)";
-  //     case "dark":
-  //       return "rgba(0, 0, 0, 0.5)";
-  //     case "electric":
-  //       return "rgba(226,227,43 0.5)";
-  //     case "fighting":
-  //       return "rgba(153,64,37 ,0.5)";
-  //     case "fire":
-  //       return "rgba(171,31,36 ,0.5)";
-  //     case "ghost":
-  //       return "rgba( 51,51,107,0.5)";
-  //     case "grass":
-  //       return "rgba( 20,123,61, 0.5)";
-  //     case "ground":
-  //       return "rgba(168,112,45, 0.5)";
-  //     case "normal":
-  //       return "rgba(117,82,92, 0.5)";
-  //     case "poison":
-  //       return "rgba(94,45,137, 0.5)";
-  //     case "psychic":
-  //       return "rgba(165,42,108, 0.5)";
-  //     case "rock":
-  //       return "rgba(72,25,11, 0.5)";
-  //     case "water":
-  //       return "rgba(21,82,225, 0.5)";
-  //     default:
-  //       return "";
-  //   }
-  // }
-
+  console.log(pokemon.types[0].type.name)
   return (
     <>
       {isOpenModal && <Backdrop onCancel={toggleModal} />}
       {isOpenModal && <Modal pokemon={pokemon} />}
       <div
-        className={styles.container}
+        className={`${styles.container} ${pokemon?.types[0]?.type?.name}`}
         onClick={toggleModal}
       >
         <p>
